eb2899e1fc6c27e307d0d01c43711a2b725a8e91
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-07-04 16:34:32 -0400

    Added data to .env hardhat.config.js files

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/MY_INFURA_PROJECT_ID",
+      url: "https://sepolia.infura.io/v3/incident-logger",
       accounts: ["a343641c82d99e61589efb4a75380ed9a876ad681fb3b87879f49ed6c05c2ed9"]
     }
   }
